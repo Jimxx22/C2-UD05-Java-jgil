fd83c8ff9d97f4efb2cda4df9db93da9a0a22a7c
Jimxx22 <jaumegil22@gmail.com>
2022-07-07 11:25:01 +0200

    Subir ejercicio 1

---
+++ UD05-01/src/MainApp.java
@@ -0,0 +1,17 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int a= 5, b=5;
+		
+		if(a<b) {
+			System.out.println("El número "+a+" es menor a "+b);
+		}else if(a>b) {
+			System.out.println("El número "+a+" es mayor a "+b);
+		}else {
+			System.out.println("Los números "+a+" y "+b+" son iguales.");
+		}
+
+	}
+
+}
