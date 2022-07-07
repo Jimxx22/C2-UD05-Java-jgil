7dcd5dfa1f5a50bbd3452aba4873afbd5cd1f232
Jimxx22 <jaumegil22@gmail.com>
2022-07-07 11:26:00 +0200

    Subir ejercicio 3

---
+++ UD05-03/src/MainApp.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class MainApp {
+
+	public static void main(String[] args) {
+		String nombre=JOptionPane.showInputDialog("Introduce tu nombre: ");
+		
+		JOptionPane.showMessageDialog(null, "Binevenido "+nombre+"!!");
+
+
+	}
+
+}
